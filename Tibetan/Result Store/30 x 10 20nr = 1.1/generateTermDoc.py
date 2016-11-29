fab2acf471ccbae21947949679b9f0e391317bf9
Prateek Agarwal <prateek.agarwal3@gmail.com>
2016-11-29 12:43:31 +0530

    Tibetan Finally Happening

---
+++ Tibetan/Result Store/30 x 10 20nr = 1.1/generateTermDoc.py
@@ -0,0 +1,55 @@
+import PIL
+import os
+import math
+from PIL import Image
+import numpy as np
+import random
+import sys
+
+
+
+Strokes = 20
+
+M = 1600
+Eps = 0.0000000001
+
+
+zers = "0000"
+termDocument = np.zeros((Samples,M))
+docNum = 0
+
+for dirTrav in xrange(1) : # 4 - 33 Directories
+	
+
+	
+	#print "File : " + str(dirLength)
+	try : 
+		for fileTrav in xrange(Samples): #Each Image of a Character		
+			imgPath ="StrokeImage0L"+str(fileTrav)+".tif";
+			print imgPath
+			#N = N + 1
+			try:
+				img = Image.open(imgPath)
+			except (IOError,OSError) as err:
+				print "Image Not Found"
+				continue
+			imgPixels = np.array(img) #255 is White & 0 is Black
+			tempImgPixels = [0 for x in range(1600)]
+			for x in xrange(0,40):
+				for y in xrange(0,40):
+					tempImgPixels[x*40+y] = imgPixels[x][y]
+			termDocument[docNum] = tempImgPixels
+			print termDocument[docNum]
+			docNum = docNum + 1
+
+	except(IOError,OSError) as err:
+		print "error"
+		continue
+
+#Stored Term Document matrix in pickle
+
+np.save("termDocStroke20nr11",termDocument)
+ter2 = np.load("termDocStroke20nr11.npy")
+if (ter2 == termDocument).all():
+	print "Success"
+#print N
--- Tibetan/ocr_own_dot.py
+++ Tibetan/ocr_own_dot.py
@@ -22,6 +22,7 @@ Strokes = charMap.shape[1]
 zers = "0000"
 
 Samples = 10
+existingsamples = 0
 imgPixels = np.zeros((M),dtype = np.float64)
 
 def calcSim(A,B):
@@ -32,37 +33,42 @@ for dirpath in xrange(4,34):
 	charPath = "F" + zers[:3-len(str(dirpath))] + str(dirpath)
 
 	correct = 0.0
-	for i in xrange(11,21):
+	for i in xrange(29,39):
 		path = "Testing Data/"+str(charPath)+"/"+"000" + str(i)+".tif";
 		print path
-		img = Image.open(path);
-		imgArray = np.array(img);
-		for k in xrange(rM):
-			for l in xrange(rM):
-				imgPixels[k*rM+l] = imgArray[k][l];
-		imgPixels = 255 - (imgPixels)
-		imgPixels =  imgPixels/255
-		
-		mn = 1000000000000000
-		pos = -1
-		for j in xrange(charNum):
-			dist = 0
-			C = np.zeros((Strokes),dtype = np.float64)
-			sm = np.sum(imgPixels)
-			for k in xrange(Strokes):
-				for l in xrange(M):
-					C[k]+=calcSim(imgPixels[l],P[k][l])
-				C[k]= C[k]/sm
-			for k in xrange(Strokes):
-				dist = (C[k]-charMap[j][k])**2 + dist
-			if(mn>dist):
-				pos = j
-				mn = dist
+		try :	
+			img = Image.open(path);
+			imgArray = np.array(img);
+			for k in xrange(rM):
+				for l in xrange(rM):
+					imgPixels[k*rM+l] = imgArray[k][l];
+			imgPixels = 255 - (imgPixels)
+			imgPixels =  imgPixels/255
+			
+			mn = 1000000000000000
+			pos = -1
+			for j in xrange(charNum):
+				dist = 0
+				C = np.zeros((Strokes),dtype = np.float64)
+				sm = np.sum(imgPixels)
+				for k in xrange(Strokes):
+					for l in xrange(M):
+						C[k]+=calcSim(imgPixels[l],P[k][l])
+					C[k]= C[k]/sm
+				for k in xrange(Strokes):
+					dist = (C[k]-charMap[j][k])**2 + dist
+				if(mn>dist):
+					pos = j
+					mn = dist
 
-		#print pos,dist
-		if pos == dirpath-4 :
-			correct = correct + 1
+			#print pos,dist
+			existingsamples = existingsamples + 1
+			if pos == dirpath - 4 :
+				correct = correct + 1
+		except(IOError,OSError):
+			print "Image Not Found"
+
 	correct_all = correct_all + correct
 	print correct*100/Samples
 
-print "Overall accuracy : "+str(correct_all*100/(charNum*Samples))
+print "Overall accuracy : "+str(correct_all*100/existingsamples)
